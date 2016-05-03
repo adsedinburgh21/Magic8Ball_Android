800e34e7fd0c49a95b0eb345a956f9c4a3355111
Adam Reid <adamreid21@hotmail.co.uk>
2016-05-03 16:27:42 +0100

    added animations- ball spins and zooms in and out and textview fades in, and sound-harp when ball clicked

--- app/src/main/java/adamreid/codeclan/com/magic8ball/Magic8Ball.java
+++ app/src/main/java/adamreid/codeclan/com/magic8ball/Magic8Ball.java
@@ -1,9 +1,13 @@
 package adamreid.codeclan.com.magic8ball;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -36,7 +40,9 @@ public class Magic8Ball extends AppCompatActivity{
 
                 mDisplayResponse = (TextView) findViewById(R.id.response_eight_ball);
                 mDisplayResponse.setText( generateResponse() );
-
+                fade(view);
+                rotate(view);
+                playSound();
             }
         });
     }
@@ -69,6 +75,9 @@ public class Magic8Ball extends AppCompatActivity{
         Log.d("Magic8Ball:", question);
 
 
+        if( question.matches("")){
+            return "Ask a question above";
+        }
         if( question.toLowerCase().contains("lottery")){
             return "You have a 1 in 45,057,474 chance of winning the lottery";
         }
@@ -78,12 +87,42 @@ public class Magic8Ball extends AppCompatActivity{
         if ((question.toLowerCase().contains("greatest") || question.toLowerCase().contains("best")) && question.toLowerCase().contains("footballer")){
             return "Its either Zinedine Zidane or James McFadden";
         }
+        if ( question.toLowerCase().contains("messi") && question.toLowerCase().contains("ronaldo")){
+            return "Clearly, Messi is better. Ronaldo moans too much";
+        }
         else {
             Random random = new Random();
             int result = random.nextInt(mResponses.size());
             return mResponses.get(result);
         }
     }
+
+
+
+
+
+    public void fade(View view){
+        Animation fade = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+        mDisplayResponse.startAnimation(fade);
+    }
+
+    public void rotate(View view){
+        Animation rotate = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate);
+        mButtonEightBall.startAnimation(rotate);
+    }
+
+    public void playSound(){
+        MediaPlayer sound = MediaPlayer.create(this, R.raw.magic_harp);
+        sound.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        sound.start();
+    }
+
+
+
+
+
+
+
 }
 
 
